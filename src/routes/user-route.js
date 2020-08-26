403ac99be4e520f6c145ee24bbc89b9a501afcd8
Hong <hkhong91@gmail.com>
2020-08-26 18:12:09 +0900

    CustomError

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const userService = require('../services/user-service')
+const CustomError = require('../advice/custom-error')
 
 router.post('/signin', async (req, res) => {
   let { email, password } = req.body
@@ -9,6 +10,9 @@ router.post('/signin', async (req, res) => {
     let user = await userService.signin(email, password)
     return res.json(user)
   } catch (err) {
+    if (err instanceof CustomError) {
+      return res.status(err.code).json(err)
+    }
     return res.status(500).json(err)
   }
 })
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,21 +1,18 @@
 const pool = require('../datas/pool')
 const bcrypt = require('bcrypt')
+const CustomError = require('../advice/custom-error')
 
 exports.signin = async (email, password) => {
-  try {
-    let data = await pool.query(`select * from User where email = ?`, [email])
-    let users = data[0]
-    if (users.length == 0) {
-      throw Error('없음')
-    }
-
-    let user = users[0]
-    if (!await bcrypt.compare(password, user.password)) {
-      throw Error('불일치')
-    }
-
-    return user
-  } catch (err) {
-    throw Error(err)
+  const data = await pool.query(`select * from User where email = ?`, [email])
+  const users = data[0]
+  if (users.length == 0) {
+    throw new CustomError(404, '사용자를 찾을 수 없습니다.')
   }
+
+  let user = users[0]
+  if (!await bcrypt.compare(password, user.password)) {
+    throw new CustomError(404, '패스워드가 다릅니다.')
+  }
+
+  return user
 }
