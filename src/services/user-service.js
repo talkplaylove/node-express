1908536e8c2f50875d2e63ee50f6ead7a88957db
Hong <hkhong91@gmail.com>
2020-08-30 19:57:40 +0900

    query code arrange

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -3,7 +3,20 @@ const bcrypt = require('bcrypt')
 const CustomError = require('../advice/custom-error')
 
 exports.signin = async (email, password) => {
-  const data = await pool.query(`select * from User where email = ?`, [email])
+  const data = await pool.query(`
+    select 
+      id,
+      content,
+      title,
+      deleted,
+      hitCount,
+      userId,
+      createdAt,
+      updatedAt
+    from User 
+    where email = ?`,
+    [email]
+  )
   const users = data[0]
   if (users.length == 0) {
     throw new CustomError(404, '사용자를 찾을 수 없습니다.')
@@ -19,14 +32,16 @@ exports.signin = async (email, password) => {
 
 exports.signup = async (user) => {
   const currentDate = new Date()
-  const data = await pool.query(`insert into User set ?`, {
-    name: user.name,
-    password: bcrypt.hashSync(user.password, 10),
-    email: user.email,
-    gender: user.gender,
-    createdAt: currentDate,
-    updatedAt: currentDate
-  })
+  const data = await pool.query(`insert into User set ?`,
+    {
+      name: user.name,
+      password: bcrypt.hashSync(user.password, 10),
+      email: user.email,
+      gender: user.gender,
+      createdAt: currentDate,
+      updatedAt: currentDate
+    }
+  )
   const result = data[0]
   return {
     id: result.insertId
@@ -34,14 +49,24 @@ exports.signup = async (user) => {
 }
 
 exports.duplicateEmail = async (email) => {
-  const data = await pool.query(`select count(1) as duplicated from User where email = ?`, [email])
+  const data = await pool.query(`
+    select count(1) as duplicated 
+    from User 
+    where email = ?`,
+    [email]
+  )
   return {
     duplicated: data[0][0].duplicated > 0 ? true : false
   }
 }
 
 exports.duplicateName = async (name) => {
-  const data = await pool.query(`select count(1) as duplicated from User where name = ?`, [name])
+  const data = await pool.query(`
+    select count(1) as duplicated
+    from User
+    where name = ?`,
+    [name]
+  )
   return {
     duplicated: data[0][0].duplicated > 0 ? true : false
   }
