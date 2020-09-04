3b8c32fbd334d621cfe4fa5c1a9fd5c0aa31364b
Hong <hkhong91@gmail.com>
2020-09-04 19:59:13 +0900

    error response body

--- src/advice/error-handler.js
+++ src/advice/error-handler.js
@@ -2,7 +2,9 @@ const CustomError = require('./custom-error')
 
 module.exports = (err, req, res, next) => {
   if (err instanceof CustomError) {
-    return res.status(err.code).json(err)
+    return res.status(err.code).json({
+      message: err.message
+    })
   }
   console.log(err)
   return res.status(500).json(err)
